47b4cbca7be6f68d8fd5c720c9da94fb8b976973
Anikait Mundhra <ani@169-231-65-193.wireless.ucsb.edu>
2023-04-25 14:39:42 -0700

    AM - added bookFixtures.js and examples in teh file

---
+++ frontend/src/fixtures/bookFixtures.js
@@ -0,0 +1,42 @@
+const bookFixtures = {
+    oneBook:
+    [
+      {
+       "id": 1,
+        "name": "The Martian",
+        "author": "Andy Weir",
+        "genre": "Historical Fiction",
+        "word count": "104588",   
+      }
+    ],
+
+    threeBooks:
+    [
+        {
+            "id": 2,
+             "name": "Percy Jackson and the Lightning Thief",
+             "author": "Rick Riordan",
+             "genre": "Nonfiction",
+             "word count": "87223",     
+        },
+
+        {
+            "id": 3,
+             "name": "Parameterized Algorithms",
+             "author": "Daniel Lokshtanov",
+             "genre": "Fantasy",
+             "word count": "too many",     
+        },
+
+        {
+            "id": 4,
+             "name": "The Hobbit",
+             "author": "J. R. R. Tolkien",
+             "genre": "Science Fiction",
+             "word count": "95356",     
+        },
+        
+    ]
+};
+
+export { bookFixtures };
