faab44f3972b7b5cedf42f20d20c6f582ae48ea2
mike-k999 <72236968+mike-k999@users.noreply.github.com>
2023-04-27 15:59:34 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,10 @@ import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import RestaurantDetailsPage from "main/pages/Restaurants/RestaurantDetailsPage";
 
 
-// import TransportCreatePage from "main/pages/Transports/TransportCreatePage";
+import TransportCreatePage from "main/pages/Transports/TransportCreatePage";
 import TransportEditPage from "main/pages/Transports/TransportEditPage";
 import TransportIndexPage from "main/pages/Transports/TransportIndexPage";
-// import TransportDetailsPage from "main/pages/Transports/TransportDetailsPage";
+import TransportDetailsPage from "main/pages/Transports/TransportDetailsPage";
 
 
 
@@ -49,10 +49,9 @@ function App() {
         <Route exact path="/restaurants/details/:id" element={<RestaurantDetailsPage />} />
         <Route exact path="/restaurants/" element={<RestaurantIndexPage />} />
 
-
-        {/* <Route exact path="/transports/create" element={<TransportCreatePage />} /> */}
+        <Route exact path="/transports/create" element={<TransportCreatePage />} />
         <Route exact path="/transports/edit/:id" element={<TransportEditPage />} />
-        {/* <Route exact path="/transports/details/:id" element={<TransportDetailsPage />} /> */}
+        <Route exact path="/transports/details/:id" element={<TransportDetailsPage />} />
         <Route exact path="/transports/" element={<TransportIndexPage />} />
 
         <Route exact path="/attractions/create" element={<AttractionCreatePage />} />
@@ -60,13 +59,10 @@ function App() {
         <Route exact path="/attractions/details/:id" element={<AttractionDetailsPage />} />
         <Route exact path="/attractions/" element={<AttractionIndexPage />} />
 
-
         <Route exact path="/books/create" element={<BookCreatePage />} />
         <Route exact path="/books/edit/:id" element={<BookEditPage />} />
         <Route exact path="/books/details/:id" element={<BookDetailsPage />} />
         <Route exact path="/books/" element={<BookIndexPage />} />
-
-
       </Routes>
     </BrowserRouter>
   );
